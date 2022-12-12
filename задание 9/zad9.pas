f827fee5799dad7b10389a577735b6d46963c28f
Kirill <117730839+Ett1s@users.noreply.github.com>
2022-12-12 19:23:17 +0300

    Add files via upload

---
+++ задание 9/zad9.pas
@@ -0,0 +1,18 @@
+﻿var f,g:text;
+    s:string;
+begin
+assign(f,'111.txt');
+reset(f);
+assign(g,'112.txt');
+rewrite(g);
+while not eof(f) do
+ begin
+  readln(f,s);
+  while length(s)<80 do
+  s:=' '+s+' ';
+  writeln(g,s);
+ end;
+close(g);
+close(f);
+
+end.
